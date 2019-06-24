4f9c838e7cf160054841a2456b87e88d085a9f27
Ammar Razaman <imdakmar@gmail.com>
2019-06-24 15:27:31 +0800

    Dual language for bahasa

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,12 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('identity_card')->unique();
+            $table->string('image_url')->nullable();
+            $table->string('phone');
+            $table->enum('type',[1,2,3]);
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
---
+++ resources/lang/en/placeholder.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dakmar
+ * Date: 24/06/2019
+ * Time: 3:20 PM
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Placeholder Language Lines - English
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the Placeholder.
+    |
+    */
+
+
+    'name' => 'Name',
+
+];
